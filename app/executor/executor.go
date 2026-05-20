7d6054fd96d3a671d772aceaab344532b0b04edc
Sampath Angara <sampathangara123@gmail.com>
2026-05-20 22:00:53 +0530

    codecrafters submit [skip ci]

--- app/executor/executor.go
+++ app/executor/executor.go
@@ -23,16 +23,19 @@ func echoFunc(args string) interface{} {
 	args = strings.Trim(args, " ")
 	deconstructedArgs := strings.Split(args, "'")
 	var finalArr []string
+	
 	for i := 0; i < len(deconstructedArgs) - 1; i = i + 2 {
-		temp := strings.Fields(deconstructedArgs[i])
+		temp := strings.Split(deconstructedArgs[i], " ")
 		finalArr = append(finalArr, strings.Join(temp, " "))
 		finalArr = append(finalArr, deconstructedArgs[i + 1])
 	}
-	temp := strings.Fields(deconstructedArgs[len(deconstructedArgs) - 1])
+	temp := strings.Split(deconstructedArgs[len(deconstructedArgs) - 1], " ")
 	finalArr = append(finalArr, strings.Join(temp, " "))
 	fmt.Println(strings.Join(finalArr, ""))
 	return nil
 }
+
+// echo 'world     script' 'example''hello' test''shell
 
 func typeFunc(args string) interface{} {
 	deconstructedArgs := strings.Fields(args)
